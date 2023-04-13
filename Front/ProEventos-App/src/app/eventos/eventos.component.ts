e8d6a89d6c0114e94fe8680deb9e882bb3fb8464
Eriko Sampaio <erikoa.93@gmail.com>
2023-04-12 22:05:45 -0300

    Usando ngIf para mostrar dados apenas se existirem.

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -10,17 +10,17 @@ export class EventosComponent implements OnInit {
 
   constructor(private http: HttpClient) { }
 
-  public eventos: any;
+  public eventos: any = [];
 
   ngOnInit(): void {
     this.getEventos();
   }
 
   public getEventos(): void {
-    this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response,
-      error => console.log(error)
-    );
+    // this.http.get('https://localhost:5001/api/eventos').subscribe(
+    //   response => this.eventos = response,
+    //   error => console.log(error)
+    // );
   }
 
 }
