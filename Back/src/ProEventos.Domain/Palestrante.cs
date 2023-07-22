80062e8cb74206c2a1d466c60b3aa32fbfe4b442
Eriko Sampaio <erikoa.93@gmail.com>
2023-07-22 20:25:01 -0300

    Corrijo prop emnail para email.

--- Back/src/ProEventos.Domain/Palestrante.cs
+++ Back/src/ProEventos.Domain/Palestrante.cs
@@ -9,7 +9,7 @@ namespace ProEventos.Domain
         public string MiniCurriculo { get; set; }
         public string ImagemURL { get; set; }
         public string Telefone { get; set; }
-        public string Enmail { get; set; }
+        public string Email { get; set; }
         public IEnumerable<RedeSocial> RedesSociais { get; set; }
 
         public IEnumerable<PalestranteEvento> PalestrantesEventos { get; set; }
