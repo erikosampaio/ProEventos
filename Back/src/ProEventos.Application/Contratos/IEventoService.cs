c1e4c7e564ae509da863f2dcc958ac935fe7ee25
Eriko Sampaio <erikoa.93@gmail.com>
2023-07-25 17:10:01 -0300

    Contrato Evento Application.

--- Back/src/ProEventos.Application/Contratos/IEventoService.cs
+++ Back/src/ProEventos.Application/Contratos/IEventoService.cs
@@ -1,10 +1,13 @@
 using System.Threading.Tasks;
 using ProEventos.Domain;
 
-namespace ProEventos.Persistence.Contratos
+namespace ProEventos.Application.Contratos
 {
-    public interface IEventoPersist
+    public interface IEventoService
     {
+        Task<Evento> AddEvento(Evento model);
+        Task<Evento> UpdateEvento(int eventoId, Evento model);
+        Task<bool> DeleteEvento(int eventoId);
         Task<Evento[]> GetAllEventosAsync(bool includePalestrantes = false);
         Task<Evento> GetEventoByIdAsync(int EventoId, bool includePalestrantes = false);
         Task<Evento[]> GetAllEventosByTemaAsync(string tema, bool includePalestrantes = false);
