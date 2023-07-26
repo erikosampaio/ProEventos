b86959eef31a3c35cd412719b8277a50d7e4bc55
Eriko Sampaio <erikoa.93@gmail.com>
2023-07-26 14:43:48 -0300

    Teste - As No Tracking.

--- Back/src/ProEventos.Persistence/EventoPersist.cs
+++ Back/src/ProEventos.Persistence/EventoPersist.cs
@@ -14,6 +14,7 @@ namespace ProEventos.Persistence
     public EventoPersist(ProEventosContext context)
     {
       _context = context;
+      // _context.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
     }
 
     public async Task<Evento[]> GetAllEventosAsync(bool includePalestrantes = false)
@@ -29,7 +30,7 @@ namespace ProEventos.Persistence
                 .ThenInclude(pe => pe.Palestrante);
         }
 
-        query = query.OrderBy(e => e.Id);
+        query = query.AsNoTracking().OrderBy(e => e.Id);
 
         return await query.ToArrayAsync();
     }
@@ -47,7 +48,8 @@ namespace ProEventos.Persistence
                 .ThenInclude(pe => pe.Palestrante);
         }
 
-        query = query.OrderBy(e => e.Id)
+        query = query.AsNoTracking()
+                     .OrderBy(e => e.Id)
                      .Where(e => e.Tema.ToLower().Contains(tema.ToLower()));
 
         return await query.ToArrayAsync();
@@ -66,7 +68,8 @@ namespace ProEventos.Persistence
                 .ThenInclude(pe => pe.Palestrante);
         }
 
-        query = query.OrderBy(e => e.Id)
+        query = query.AsNoTracking()
+                     .OrderBy(e => e.Id)
                      .Where(e => e.Id == eventoId);
 
         return await query.FirstOrDefaultAsync();
